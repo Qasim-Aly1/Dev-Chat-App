2579e3e7aeb8e404dc88a75bb3ad6ada6c36ad34
Qasim-Aly1 <qaskiyar99@gmail.com>
2025-01-11 17:40:47 +0500

    Initial commit for express server routes

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+// Used to store the MongoDB connection string
+module.exports = {  
+    mongoURI: 'mongodb+srv://qasim:DfLUL4iGEnqWi6AD@cluster0.gkvh4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+}
